51df48bac93fb748565ba863ebbdb28324e7a2c4
ZetaXII <mario.zaccardi12@gmail.com>
2026-03-07 02:58:39 +0100

    index setup, reordered some styles, added services section and more fixes

---
+++ assets/js/loadComponents.js
@@ -0,0 +1,23 @@
+fetch('/components/navbar.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    });
+
+fetch('/components/footer.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+fetch('/components/hero.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('hero').innerHTML = data;
+    });
+
+fetch('/components/servizi.html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('servizi').innerHTML = data;
+    });
