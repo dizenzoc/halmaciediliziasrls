145ab962eb1e6ad610437ba58335ec29d7f0f67b
ZetaXII <mario.zaccardi12@gmail.com>
2026-03-21 01:11:41 +0100

    aggiunta animazione allo scrolling

---
+++ assets/js/animations.js
@@ -0,0 +1,16 @@
+function animationReveal() {
+    const elements = document.querySelectorAll('.reveal');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.3
+    });
+
+    elements.forEach(el => observer.observe(el));
+}
--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -3,36 +3,42 @@ fetch('/components/navbar.html')
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
         collapseNavbarWhenClick();
+        animationReveal();
     });
 
 fetch('/components/footer.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
+        animationReveal();
     });
 
 fetch('/components/hero.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('home').innerHTML = data;
+        animationReveal();
     });
 
 fetch('/components/services.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('services').innerHTML = data;
+        animationReveal();
     });
 
 fetch('/components/aboutUs.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('about-us').innerHTML = data;
+        animationReveal();
     });
 
 fetch('/components/contact.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('contact-us').innerHTML = data;
+        animationReveal();
         initSwitchForms();
         initWorkWithUsForm();
     });
@@ -41,6 +47,6 @@ fetch('/components/userReviews.html')
     .then(res => res.text())
     .then(data => {
         document.getElementById('user-reviews').innerHTML = data;
-
+        animationReveal();
         initWorkWithUsForm();
     });
